9fb0ca4468bca55a451d80bae2f1aee6c6bb35c2
Tadeas Hort <tadeashort@centrum.cz>
2020-10-19 22:38:32 +0200

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ac1eae4ff103aaf1d5702aabe757274e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b2c14aa1-6faf-4004-9779-183dbc97b0fc'
+};
